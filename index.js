812fa8fd2eae1f5cf7053c25a3031214ff641ef1
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-03 09:37:10 -0700

    Implement GET endpoints to retrieve all projects and/or a single project

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ const actionDb = require('./data/helpers/actionModel');
 const server = express();
 server.use(express.json());
 
-server.use('/', (req, res) => res.send('API up and running!'));
+//===========Projects===========
+
+server.get('/api/projects', (req, res) => {
+  projectDb.get()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500)
+        .json({ error: "The projects information could not be retrieved"});
+    });
+});
+
+server.get('/api/projects/:id', (req, res) => {
+  projectDb.get(req.params.id)
+    .then(response => {
+      if (!response) {
+        res.status(404)
+          .json({ message: "The project with the specified ID does not exist" });
+      } else {
+        res.status(200).json(response);
+      } 
+    })
+    .catch(err => {
+      res.status(500)
+        .json({ error: "The project information could not be retrieved"});
+    });
+});
 
 server.listen(8000, () => console.log('API running on port 8000'));
